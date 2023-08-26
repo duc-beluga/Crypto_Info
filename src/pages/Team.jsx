0de28956c3475d8247e7c6110f8251acc28ee729
Duc Nguyen <dnguyen416@gatech.edu>
2023-08-26 15:07:28 -0400

    Navbar on TeamPage (Ticket: Add Navbar as wrapper)

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -8,30 +8,34 @@ import {
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Team = () => {
   const members = ["Noah Nguyen", "Awonke Mnotoza"];
   const linkedIn = ["https://www.linkedin.com/", "https://www.linkedin.com/"];
   const github = ["https://github.com/", "https://github.com/"];
   return (
-    <div className="bg-gradient-to-b from-[#F0DCB1] h-screen flex flex-col pr-4 justify-center items-center">
-      <ul>
-        {members.map((mem, ind) => (
-          <li key={ind} className="p-2 font-semibold">
-            <div className="flex flex-col">
-              <div className="flex">
-                <p className="p-2">{mem}</p>
-                <Link to={linkedIn[ind]} className="p-2">
-                  <FontAwesomeIcon icon={faLinkedin} size="x" />
-                </Link>
-                <Link to={github[ind]} className="p-2">
-                  <FontAwesomeIcon icon={faGithub} size="x" />
-                </Link>
+    <div className="bg-gradient-to-b from-[#F0DCB1] h-screen flex flex-col pr-4">
+      <Navbar />
+      <div className="bg-gradient-to-b from-[#F0DCB1] h-screen flex flex-col pr-4 justify-center items-center">
+        <ul>
+          {members.map((mem, ind) => (
+            <li key={ind} className="p-2 font-semibold">
+              <div className="flex flex-col">
+                <div className="flex">
+                  <p className="p-2">{mem}</p>
+                  <Link to={linkedIn[ind]} className="p-2">
+                    <FontAwesomeIcon icon={faLinkedin} size="x" />
+                  </Link>
+                  <Link to={github[ind]} className="p-2">
+                    <FontAwesomeIcon icon={faGithub} size="x" />
+                  </Link>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
