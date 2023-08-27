465c86ef822470940e03187c1f372408917dc389
Awonke11 <93478189+Awonke11@users.noreply.github.com>
2023-08-27 06:15:07 +0200

    UI Update and Feature Update

--- src/components/AnalyticsCard.jsx
+++ src/components/AnalyticsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AnalyticsCard = () => {
   return (
-    <div className="flex flex-grow m-4 border border-slate-300 rounded-lg shadow-xl bg-white items-center justify-center">
+    <div className="flex flex-grow h-[30%] border border-slate-300 rounded-lg shadow-xl bg-white items-center justify-center">
       Analytics Card
     </div>
   );
--- src/components/SwapCard.jsx
+++ src/components/SwapCard.jsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect, useRef } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect, useContext } from "react";
 import getPrice from "../swap2";
+import { AppContext } from "../context/AppContext";
+import { BarLoader } from "react-spinners";
 
 const SwapCard = () => {
   const [output, setOutput] = useState(0);
   const [inputAmount, setInputAmount] = useState(0);
   const [inputTok, setInputTok] = useState("WBTC");
   const [outputTok, setOutputTok] = useState("ETH");
+  const [loadingOutput, setLoadingOutput] = useState(false)
+
+  const {setSearchNewsInput, searchNewsInput} = useContext(AppContext)
 
   const handleChange = (e) => {
     setInputAmount(e.target.value);
@@ -13,19 +19,27 @@ const SwapCard = () => {
 
   useEffect(() => {
     const fetchPrice = async () => {
+      setLoadingOutput(true)
       const res = await getPrice(inputAmount, inputTok, outputTok);
       setOutput(Math.round(res * 100000) / 100000);
+      setLoadingOutput(false)
     };
-    if (inputAmount) {
+    if (inputAmount > 0) {
       fetchPrice();
+    } else {
+      setLoadingOutput(false)
+      setOutput(0)
     }
-  }, [inputAmount]);
+    if (searchNewsInput.trim().length === 0) {
+      setSearchNewsInput(inputTok)
+    }
+  }, [inputAmount, inputTok, outputTok, searchNewsInput, setSearchNewsInput]);
   return (
-    <div className="h-72 m-5 mb-3 border border-slate-300 rounded-lg shadow-xl flex flex-col p-2 bg-white">
-      <div>Swap Icon</div>
-      <div className="border border-slate-300 m-2 h-2/5 rounded-lg p-2">
+    <div className="h-[70%] border border-slate-300 rounded-2xl shadow-xl flex flex-col bg-white p-4 gap-4">
+      <h2 className="text-lg font-bold">Swap Icon</h2>
+      <div className="bg-[#F2F2F2] h-2/5 rounded-xl p-3">
         <div className="flex justify-between">
-          <div className="text-xs">You pay</div>
+          <h3 className="text-sm text-[#A3A3A3]">You pay</h3>
           <select
             name="Coin"
             className="bg-[#F0DCB1] rounded-md focus:outline-none text-xs"
@@ -38,14 +52,14 @@ const SwapCard = () => {
           </select>
         </div>
         <input
-          className="text-2xl  w-full rounded-md focus:outline-none"
+          className="text-4xl mt-1 w-full rounded-md focus:outline-none bg-transparent"
           placeholder="0"
           onChange={handleChange}
         />
       </div>
-      <div className="border border-slate-300 m-2 h-2/5 rounded-lg p-2">
+      <div className="bg-[#F2F2F2] h-2/5 rounded-xl p-3">
         <div className="flex justify-between">
-          <div className="text-xs">You receive</div>
+          <h3 className="text-sm text-[#A3A3A3]">You receive</h3>
           <select
             name="Coin"
             className="bg-[#F0DCB1] rounded-md focus:outline-none text-xs"
@@ -53,9 +67,14 @@ const SwapCard = () => {
             <option value="1">ETH</option>
           </select>
         </div>
-        <p className="text-2xl">{output}</p>
+        <p className="text-4xl mt-1 w-full rounded-md focus:outline-none bg-transparent">{
+          loadingOutput ? <BarLoader 
+            color="#EC8E00" 
+            className="mt-6 w-full"
+          /> : output
+        }</p>
       </div>
-      <button className="h-1/5 border border-slate-300 rounded-lg ml-2 mr-2 bg-[#F0DCB1]">
+      <button className="h-1/5 border-none rounded-lg bg-[#FFECCF] text-[#EC8E00] font-bold">
         Connect Wallet
       </button>
     </div>
